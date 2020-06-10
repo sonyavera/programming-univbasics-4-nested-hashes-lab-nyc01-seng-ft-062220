56dd4212cedefb860731af685c772574ea3c74ba
Sonya Gould <sonyavgould@gmail.com>
2020-06-10 20:44:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -35,7 +35,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -54,7 +54,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for]
+
 end
 
 def adding_matz
