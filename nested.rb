6cfddaff83941d3b7a6a85cb45aad96a997ddfef
Sonya Gould <sonyavgould@gmail.com>
2020-06-10 20:44:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_kay][:known_for]
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -55,7 +54,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_matz
